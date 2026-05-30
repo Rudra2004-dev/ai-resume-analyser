951c754363ceac535eae790b45a2d845721e3510
Rudra Prosad Ghosh <rudraprosadghosh627@gmail.com>
2026-05-30 23:49:35 +0530

    Added ResumeCard, ScoreCircle

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import {resumes} from "../../constants"
 import Navbar from "../components/Navbar";
-import bgMain from "../design/images/bg-main.svg"
+import ResumeCard from "../components/ResumeCard";
+import bgMain from "/images/bg-main.svg"
 
 const Home = () => {
   return (
@@ -9,9 +11,16 @@ const Home = () => {
         <Navbar/>
         <section className="main-section">
             <div className="page-heading">
-                <h1>Track Your Applications & Resume Ratings</h1>
+                <h1 className="text-2xl bg-white p-4">Track Your Applications & Resume Ratings</h1>
                 <h2>Review your submissions and check your AI-powered feedback</h2>
             </div>
+        {resumes.length > 0 && (
+          <div className="flex flex-wrap justify-center gap-6 mt-10">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume}/>
+            ))}
+          </div>
+        )}
         </section>
     </main>
   )
